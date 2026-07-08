0be634f54ea9d44dd38d042fe8e1bf0c75c8973e
Simplicity005 <arjunsanesh005@gmail.com>
2026-07-08 21:01:57 +0530

    Update index, script, styles, cleaned up code

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -6,17 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let tgY = 0;
 
   window.addEventListener("mousemove", (event) => {
-    // Target positions are adjusted based on cursor position
     tgX = event.clientX;
     tgY = event.clientY;
   });
 
   function move() {
-    // Easing calculations: 20 is the divider controlling the lagging fluidity
     curX += (tgX - curX) / 80;
     curY += (tgY - curY) / 80;
 
-    // Apply transform coordinates to the interactive element
     interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
 
     requestAnimationFrame(() => {
