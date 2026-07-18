714db14ec87b2849216d00dbb2104a1d008aa431
Simplicity005 <arjunsanesh005@gmail.com>
2026-07-18 19:06:35 +0530

    Add Navbar.jsx

---
+++ src/components/layouts/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar" id="navbar">
+      <div className="nav-center">
+        <a href="#about">About Me</a>
+        <a href="#work">My Work</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
