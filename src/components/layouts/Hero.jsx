ee72f9408ede7cc22294b99548f1fe71fb25c900
Simplicity005 <arjunsanesh005@gmail.com>
2026-07-18 19:22:52 +0530

    Update and Add Hero.jsx with Socials.jsx

---
+++ src/components/layouts/Hero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Socials from "../ui/Socials";
+
+const Hero = () => {
+  return (
+    <main className="hero-container" id="hero">
+      <div className="text-side">
+        <h3 id="top-text" className="load-anim item-1">
+          Hi, I am
+        </h3>
+        <h1 id="name-center" className="load-anim item-2">
+          Arjun Sanesh
+        </h1>
+        <div className="asterisk-socials load-anim item-3">
+          <h5 id="asterisk-explanation">* Interesting fella</h5>
+          <div className="socials">
+            <Socials
+              link="https://www.linkedin.com/in/arjun-sanesh/"
+              imgURL="src/assets/images/linkedin.png"
+              alt="linkedIn_Image"
+            />
+            <Socials
+              link="https://www.instagram.com"
+              imgURL="src/assets/images/instagram.png"
+              alt="instagram_Image"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="image-side load-anim item-4">
+        <h1 className="asterisk-big">*</h1>
+        <div className="image-container" id="hero-photo-slot">
+          <img src="../assets/images/1000027072 (1).png" alt="Arjun Sanesh" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
---
+++ src/components/ui/Socials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Socials(props) {
+  return (
+    <div className="social-container">
+      <a href={props.link}>
+        <img src={props.imgURL} className="social-img" alt={props.alt} />
+      </a>
+    </div>
+  );
+}
+
+export default Socials;
