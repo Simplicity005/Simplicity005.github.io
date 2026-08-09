54352dc8c756efb5359799d311bf567372754efb
Simplicity005 <arjunsanesh005@gmail.com>
2026-08-09 08:46:15 +0530

    Update: Add new logo

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "../../assets/Logo.png";
+import logo from "../../assets/images/logo.svg";
 
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
