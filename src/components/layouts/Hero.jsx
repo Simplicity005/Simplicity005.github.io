52e99ff5ea4663c02933132b372977c730244264
Simplicity005 <arjunsanesh005@gmail.com>
2026-08-09 12:35:02 +0530

    Update: Update projects section design

--- src/components/layouts/Hero.jsx
+++ src/components/layouts/Hero.jsx
@@ -20,34 +20,36 @@ function Hero() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <main className="hero-container" id="hero" ref={heroRef}>
-      <div className="text-side">
-        <h3 id="top-text" className="load-anim item-1">
-          Hi! I am
-        </h3>
-        <h1 id="name-center" className="load-anim item-2">
-          Arjun Sanesh.
-        </h1>
-        <div className="asterisk-socials load-anim item-3">
-          <h5 id="asterisk-explanation">* Interesting fella</h5>
-          <div className="socials">
-            <Socials
-              link="https://www.linkedin.com/in/arjun-sanesh/"
-              imgURL={linkedinIcon}
-              alt="LinkedIn"
-            />
-            <Socials
-              link="https://www.instagram.com"
-              imgURL={instagramIcon}
-              alt="Instagram"
-            />
+    <main className="hero-container" id="hero">
+      <div className="hero-parallax" ref={heroRef}>
+        <div className="text-side">
+          <h3 id="top-text" className="load-anim item-1">
+            Hi! I am
+          </h3>
+          <h1 id="name-center" className="load-anim item-2">
+            Arjun Sanesh.
+          </h1>
+          <div className="asterisk-socials load-anim item-3">
+            <h5 id="asterisk-explanation">* Interesting fella</h5>
+            <div className="socials">
+              <Socials
+                link="https://www.linkedin.com/in/arjun-sanesh/"
+                imgURL={linkedinIcon}
+                alt="LinkedIn"
+              />
+              <Socials
+                link="https://www.instagram.com"
+                imgURL={instagramIcon}
+                alt="Instagram"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="image-side load-anim item-4">
-        <Asterisk />
-        <div className="image-container" id="hero-photo-slot">
-          <img src={heroPhoto} alt="Arjun Sanesh" />
+        <div className="image-side load-anim item-4">
+          <Asterisk />
+          <div className="image-container" id="hero-photo-slot">
+            <img src={heroPhoto} alt="Arjun Sanesh" />
+          </div>
         </div>
       </div>
     </main>
