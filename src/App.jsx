805e21e0d59577aa78884b83186b03ad9ab6cebb
Simplicity005 <arjunsanesh005@gmail.com>
2026-08-09 15:29:18 +0530

    Fix blank screen on deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+/* Changed BrowserRouter to HashRouter below */
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
